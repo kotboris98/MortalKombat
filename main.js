1c27f0d8778a2597595a3a7583cde7db81388ec4
Boris <94647113+kotboris98@users.noreply.github.com>
2022-03-17 23:57:35 +0300

    homework-5

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const $arenas = document.querySelector('.arenas');
-const $randomButton = document.querySelector('.button');
+// const $randomButton = document.querySelector('.button');
+const $formFight = document.querySelector('.control');
+
+const HIT = {
+    head: 30,
+    body: 25,
+    foot: 20,
+};
+
+const ATTACK = ['head', 'body', 'foot'];
 
 const player1 = {
     player: 1,
@@ -105,16 +114,50 @@ function createReloadButton() {
     $arenas.appendChild($divReloadButton);
 }
 
-$randomButton.addEventListener('click', function() {
+/* $randomButton.addEventListener('click', function() {
     console.log('####: Click Random Button');
     player1.changeHP(getRandomNumber(20));
     player1.renderHP;
 
     player2.changeHP(getRandomNumber(20));
     player2.renderHP;
+}) */ 
 
+$arenas.appendChild(createPlayer(player1));
+$arenas.appendChild(createPlayer(player2));
+
+function enemyAttack() {
+    const hit = ATTACK[getRandomNumber(3) - 1];
+    const defence = ATTACK[getRandomNumber(3) - 1];
+    
+    return {
+        value: getRandomNumber(HIT[hit]),
+        hit,
+        defence 
+    }
+}
+  
+function playerAttack() {
+    const attack = {};
+
+    for (let item of $formFight) {
+        if (item.checked && item.name === 'hit') {
+            attack.value = getRandomNumber(HIT[item.value]);
+            attack.hit = item.value;
+    }
+
+        if (item.checked && item.name === 'defence') {
+            attack.defence = item.value;
+        }
+
+        item.checked = false;
+    }
+
+    return attack;
+}
+
+function checkResult() {
     if (player1.hp === 0 || player2.hp === 0) {
-        $randomButton.disabled = true;
         createReloadButton();
     }
 
@@ -125,7 +168,21 @@ $randomButton.addEventListener('click', function() {
     } else if (player1.hp === 0 && player2.hp === 0) {
         $arenas.appendChild(playerWins());
     }
-})
+}
 
-$arenas.appendChild(createPlayer(player1));
-$arenas.appendChild(createPlayer(player2));
+$formFight.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const enemy = enemyAttack();
+    const player = playerAttack();
+        if (enemy.hit !== player.defence) {
+            player1.changeHP(enemy.value);
+            player1.renderHP();
+        }   
+
+        if (player.hit !== enemy.defence) {
+            player2.changeHP(player.value);
+            player2.renderHP();    
+        }
+
+    checkResult();
+})
